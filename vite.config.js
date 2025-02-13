5f5edcca8e91a3b61bea5e6ce59d77a7ecfa0acb
Jonas Tobias Hopusch <git@jotoho.de>
2025-02-13 12:01:41 +0100

    fix: resolve build issues preventing vite from including all pages
    Signed-off-by: Jonas Tobias Hopusch <git@jotoho.de>

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 // SPDX-License-Identifier: CC0-1.0
 // SPDX-FileCopyrightText: 2025 Jonas Tobias Hopusch <git@jotoho.de>
 
+import { resolve } from "path";
 import { defineConfig } from 'vite';
 import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
 	plugins: [injectHTML()],
+	build: {
+		rollupOptions: {
+			input: {
+				index: resolve(__dirname, '/index.html'),
+				login: resolve(__dirname, '/login.html'),
+				uebersicht: resolve(__dirname, '/uebersicht.html'),
+			},
+		},
+		target: 'es2024',
+	},
 });
